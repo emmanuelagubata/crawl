760deffb3d3759ac7a86587a03fa9838ecf0b4e5
Jason Valadez <valadezj045@gmail.com>
2020-04-17 18:17:30 -0700

    branches

--- XMLReq.py
+++ XMLReq.py
@@ -5,6 +5,6 @@ from xml.etree import ElementTree
 
 #Do a get request using the link below. We know this link returns XML. Take that XML and convert it to JSON.
 
-keyword = "eye"
+keyword = "gold"
 
 KeywordURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&term="+ keyword +"&tool=my_tool&email=my_email@example.com"
